049affbd8b584f29a951939d3c881ccfaba017ad
Steve Klabnik <steve@steveklabnik.com>
2011-03-13 14:20:33 -0400

    dms work.

--- test/user_test.rb
+++ test/user_test.rb
@@ -9,8 +9,26 @@ class UpdateTest < MiniTest::Unit::TestCase
     update = Update.create(:text => "@steve oh hai!")
     Update.create(:text => "just some other update")
 
-    assert_equal u.at_replies.length, 1
-    assert_equal u.at_replies.first.id, update.id
+    assert_equal 1, u.at_replies.length
+    assert_equal update.id, u.at_replies.first.id
+  end
+
+  def test_dm_filter
+    u = User.create(:username => "steve")
+    update = Update.create(:text => "d steve oh hai!")
+    Update.create(:text => "just some other update")
+
+    assert_equal 1, u.dm_replies.length
+    assert_equal update.id, u.dm_replies.first.id
+  end
+
+  def test_dm_not_in_timeline
+    u = User.create(:username => "steve")
+    Update.create(:text => "d someone oh hai!", :user_id => u.id)
+    update = Update.create(:text => "just some other update", :user_id => u.id)
+
+    assert_equal 1, u.updates.length
+    assert_equal update.id, u.updates.first.id 
   end
 
 end
