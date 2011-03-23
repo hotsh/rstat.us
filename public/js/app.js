6ee67045079fd799a595449d0c5c27ded5ee6f79
Zachary Scott <zachary.s.scott@gmail.com>
2011-03-23 00:36:57 -0400

    remove original js

--- public/js/app.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $("#flash").delay(2000).slideUp('slow')
-})
--- public/js/updates.show.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(".remove-update").click(function(){
-     var r = confirm("Are you sure you want to delete this update?");
-     return r;
-  });
-});
