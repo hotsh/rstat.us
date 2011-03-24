f18b2225f4a6295f5663a8f03a5f76a6781586e4
Steve Klabnik <steve@steveklabnik.com>
2011-03-24 00:57:50 -0400

    Whoops, borked tests fixed.

--- test/user_test.rb
+++ test/user_test.rb
@@ -9,17 +9,8 @@ class UserTest < MiniTest::Unit::TestCase
     update = Update.create(:text => "@steve oh hai!")
     Update.create(:text => "just some other update")
 
-    assert_equal 1, u.at_replies.length
-    assert_equal update.id, u.at_replies.first.id
-  end
-
-  def test_dm_filter
-    u = User.create(:username => "steve")
-    update = Update.create(:text => "d steve oh hai!")
-    Update.create(:text => "just some other update")
-
-    assert_equal 1, u.dm_replies.length
-    assert_equal update.id, u.dm_replies.first.id
+    assert_equal 1, u.at_replies({}).length
+    assert_equal update.id, u.at_replies({}).first.id
   end
 
   def test_hashtag_filter
@@ -27,8 +18,8 @@ class UserTest < MiniTest::Unit::TestCase
     update = Update.create(:text => "mother-effing #hashtags")
     Update.create(:text => "just some other update")
 
-    assert_equal 1, Update.hashtag_search("hashtags").length
-    assert_equal update.id, Update.hashtag_search("hashtags").first.id 
+    assert_equal 1, Update.hashtag_search("hashtags", {}).length
+    assert_equal update.id, Update.hashtag_search("hashtags", {}).first.id 
   end
 
   def test_username_is_unique
