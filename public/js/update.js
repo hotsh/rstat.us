890d520b016683747c70255cd02c512fca986aae
Dominic Dagradi <dominic@bearded.com>
2011-03-24 17:33:10 -0400

    refactoring reply/share

--- public/js/update.js
+++ public/js/update.js
@@ -1,37 +1,39 @@
 (function() {
   $(document).ready(function() {
-    var update, updateCounter;
+    var focusTextArea, shareText, textarea, updateCounter, update_field;
     $("html").removeClass("no-js").addClass("js");
-    update = $("#update-textarea");
+    textarea = $("#update-form textarea");
+    update_field = $("#update-form #update-referral");
     updateCounter = function() {
-      $("#update-count").text((140 - update.val().length) + "/140");
-      return $("#update-info").toggleClass("negative", update.val().length > 140);
+      $("#update-count").text((140 - textarea.val().length) + "/140");
+      return $("#update-info").toggleClass("negative", textarea.val().length > 140);
     };
-    update.keypress(updateCounter).keyup(updateCounter);
-    return $("#update-form").submit(function() {
-      if (update.val().length <= 0 || update.val().length > 140) {
+    textarea.keypress(updateCounter).keyup(updateCounter);
+    $("#update-form").submit(function() {
+      if (textarea.val().length <= 0 || textarea.val().length > 140) {
         return false;
       }
     });
+    shareText = function(update) {
+      return "RT @" + $(update).data("name") + ": " + $(update).find(".text").text().trim();
+    };
+    focusTextArea = function() {
+      textarea.keypress();
+      return textarea.focus();
+    };
+    return $(".update").each(function() {
+      var update;
+      update = $(this);
+      $(this).find(".reply").bind("click", function(ev) {
+        ev.preventDefault();
+        textarea.text("@" + $(update).data("name") + " ");
+        return focusTextArea();
+      });
+      return $(this).find(".share").bind("click", function(ev) {
+        ev.preventDefault();
+        textarea.text(shareText(update));
+        return focusTextArea();
+      });
+    });
   });
 }).call(this);
-
-// TODO: MOVE TO COFFEE-SCRIPT FOO
-function reply(username) {
-  var update = $("#update-textarea");
-  update.text("@" + username + " ");
-  update.keypress();
-  update.focus();
-}
-
-function share_text(username, update_id) {
-  var update_text = $("#update-" + update_id).text().trim();
-  return "RT @" + username + ": " + update_text;
-}
-
-function share(username, update_id) {
-  var update = $("#update-textarea");
-  update.text(share_text(username, update_id));
-  update.keypress();
-  update.focus();
-}
--- src/update.coffee
+++ src/update.coffee
@@ -1,12 +1,36 @@
 $(document).ready ->
   $("html").removeClass("no-js").addClass("js")
-  update = $("#update-textarea")
+  
+  textarea = $("#update-form textarea")
+  update_field = $("#update-form #update-referral")
 
   updateCounter = ->
-    $("#update-count").text((140 - update.val().length) + "/140")
-    $("#update-info").toggleClass "negative", update.val().length > 140
+    $("#update-count").text((140 - textarea.val().length) + "/140")
+    $("#update-info").toggleClass "negative", textarea.val().length > 140
 
-  update.keypress(updateCounter).keyup(updateCounter)
+  textarea.keypress(updateCounter).keyup(updateCounter)
 
   $("#update-form").submit ->
-    false if update.val().length <= 0 || update.val().length > 140
+    false if textarea.val().length <= 0 || textarea.val().length > 140
+  
+  shareText = (update) ->
+    "RT @" + $(update).data("name") + ": " + $(update).find(".text").text().trim();
+    
+  focusTextArea = ->
+    textarea.keypress()
+    textarea.focus()
+  
+  $(".update").each ->
+    update = $(this)
+    
+    $(this).find(".reply").bind "click", (ev) ->
+      ev.preventDefault();
+      textarea.text("@" + $(update).data("name") + " ")
+      focusTextArea()
+
+    $(this).find(".share").bind "click", (ev) ->
+      ev.preventDefault();
+      textarea.text(shareText(update))
+      focusTextArea()
+      
+  
