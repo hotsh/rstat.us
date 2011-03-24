1f891708226da6f7257388c86117d06ab1f8ef46
Zachary Scott <zachary.s.scott@gmail.com>
2011-03-23 21:17:52 -0400

    just return the confirm(), no need to create local variables #webscale

--- public/js/updates.show.js
+++ public/js/updates.show.js
@@ -1,9 +1,7 @@
 (function() {
   $(document).ready(function() {
     return $(".remove-update").click(function() {
-      var r;
-      r = confirm("Are you sure you want to delete this update?");
-      return r;
+      return confirm("Are you sure you want to delete this update?");
     });
   });
 }).call(this);
