3e1ac55043418ab86a2706abad74adf567cbbefb
Shebanian <info@shebanian.com>
2011-03-27 11:50:42 +0200

    -Put the focus function in the app.coffee -Changed login -Added focus to confirm situation

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,12 @@
     return $("#flash").delay(2000).slideUp('slow');
   });
 }).call(this);
+
+function focusOnDiv( divId )
+{
+divElement = document.getElementById( divId );
+if(divElement!= null && typeof(divElement) != 'undefined')
+{
+divElement.focus();
+}
+}
