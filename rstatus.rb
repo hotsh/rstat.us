81765d350d2cbc0187dfd0fbf136f01cf4ecf89a
Dominic Dagradi <dominic@bearded.com>
2011-03-27 14:34:54 -0400

    easy username reset if username empty

--- rstatus.rb
+++ rstatus.rb
@@ -138,6 +138,42 @@ class Rstatus < Sinatra::Base
     provider :facebook, ENV["APP_ID"], ENV["APP_SECRET"]
   end
 
+  ############################
+  # EMPTY USERNAME HANDLING - quick and dirty
+  ############################
+  before do
+    @error_bar = ""
+    if current_user && (current_user.username.nil? or current_user.username.empty?)       
+      @error_bar = haml :_username_error, :layout => false
+    end
+  end
+
+  get '/reset-username' do
+    unless current_user.nil? || current_user.username.empty?
+      redirect "/"
+    end
+      
+    haml :reset_username
+  end
+
+  post '/reset-username' do
+    exists = User.first :username => params[:username]
+    if !params[:username].nil? && !params[:username].empty? && exists.nil?
+      user = current_user
+      user.username = params[:username]
+      user.author.username = params[:username]
+      user.save
+      user.author.save
+
+      flash[:notice] = "Thank you for updating your username"
+      redirect "/"
+    else
+      flash[:notice] = "Sorry, that username has already been taken or is not valid. Please try again."
+      haml :reset_username    
+    end
+  end
+  ############################
+
   get '/' do
     if logged_in?
       set_params_page
