c63de7e7ff7e8dec8699ea89cfb31b9598a8ad13
Steve Klabnik <steve@steveklabnik.com>
2011-03-27 11:57:45 -0400

    increasing cache header time on css

--- rstatus.rb
+++ rstatus.rb
@@ -175,7 +175,7 @@ class Rstatus < Sinatra::Base
   end
   
   get '/screen.css' do
-    cache_control :public, :must_revalidate, :max_age => 60
+    cache_control :public, :must_revalidate, :max_age => 360
     scss(:screen, Compass.sass_engine_options)
   end
 
