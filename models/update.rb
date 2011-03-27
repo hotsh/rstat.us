63aea65606bf9750f6b2996753d01471c38ec356
BRIMIL01 <brimil01@gmail.com>
2011-03-26 22:39:35 -0700

    HTML for update is now saved in model rather than generated every request

--- models/update.rb
+++ models/update.rb
@@ -8,7 +8,9 @@ class Update
   key :text, String, :default => ""
   key :tags, Array, :default => []
   key :language, String
-
+  
+  before_save :generate_html
+  
   attr_accessor :oauth_token, :oauth_secret
 
   validates_length_of :text, :minimum => 1, :maximum => 140
@@ -31,23 +33,7 @@ class Update
   end
 
   def to_html
-    out = CGI.escapeHTML(text)
-
-    # we let almost anything be in a username, except those that mess with urls.  but you can't end in a .:;, or !
-    #also ignore container chars [] () "" '' {}
-    # XXX: the _correct_ solution will be to use an email validator
-    out.gsub!(/(^|[ \t\n\r\f"'\(\[{]+)@([^ \t\n\r\f&?=@%\/\#]*[^ \t\n\r\f&?=@%\/\#.!:;,"'\]}\)])/) do |match|
-      if u = User.first(:username => /^#{$2}$/i)
-        "#{$1}<a href='/users/#{u.username}'>@#{$2}</a>"
-      else
-        match
-      end
-    end
-    out.gsub!(/(http[s]?:\/\/\S+[a-zA-Z0-9\/}])/, "<a href='\\1'>\\1</a>")
-    out.gsub!(/(^|\s+)#(\w+)/) do |match|
-      "#{$1}<a href='/hashtags/#{$2}'>##{$2}</a>"
-    end
-    out
+    self.html || generate_html
   end
 
   def mentioned? search
@@ -80,7 +66,27 @@ class Update
   end
 
   protected
+  
+  def generate_html
+    out = CGI.escapeHTML(text)
 
+    # we let almost anything be in a username, except those that mess with urls.  but you can't end in a .:;, or !
+    #also ignore container chars [] () "" '' {}
+    # XXX: the _correct_ solution will be to use an email validator
+    out.gsub!(/(^|[ \t\n\r\f"'\(\[{]+)@([^ \t\n\r\f&?=@%\/\#]*[^ \t\n\r\f&?=@%\/\#.!:;,"'\]}\)])/) do |match|
+      if u = User.first(:username => /^#{$2}$/i)
+        "#{$1}<a href='/users/#{u.username}'>@#{$2}</a>"
+      else
+        match
+      end
+    end
+    out.gsub!(/(http[s]?:\/\/\S+[a-zA-Z0-9\/])/, "<a href='\\1'>\\1</a>")
+    out.gsub!(/(^|\s+)#(\w+)/) do |match|
+      "#{$1}<a href='/hashtags/#{$2}'>##{$2}</a>"
+    end
+    self.html = out
+  end
+  
   def tweet
     return unless ENV['RACK_ENV'] == 'production'
     
