6b9418a6ef68dba39f585624a5d3d7bbb56a9435
Brian Miller <brimil01@gmail.com>
2011-03-27 19:53:35 -0700

    Fixed issue with twitter/facebook account auth being redirected to account confirm before authorization is saved.

--- rstatus.rb
+++ rstatus.rb
@@ -228,25 +228,10 @@ class Rstatus < Sinatra::Base
   get '/auth/:provider/callback' do
     auth = request.env['omniauth.auth']
     unless @auth = Authorization.find_from_hash(auth)
-      session[:uid] = auth['uid']
-      session[:provider] = auth['provider']
-      session[:name] = auth['user_info']['name']
-      session[:nickname] = auth['user_info']['nickname']
-      session[:website] = auth['user_info']['urls']['Website']
-      session[:description] = auth['user_info']['description']
-      session[:image] = auth['user_info']['image']
-      #let's store their oauth stuff so they don't have to re-login after
-      session[:oauth_token] = auth['credentials']['token']
-      session[:oauth_secret] = auth['credentials']['secret']
-
-      ## We can probably get rid of this since the user confirmation will check for duplicate usernames [brimil01]
-      if User.first :username => auth['user_info']['nickname'] or auth['user_info']['nickname'] =~ /profile[.]php[?]id=/
-        #we have a username conflict!
-      
-        #let's store their oauth stuff so they don't have to re-login after
-        session[:oauth_token] = auth['credentials']['token']
-        session[:oauth_secret] = auth['credentials']['secret']
-      
+      if logged_in?
+        Authorization.create_from_hash(auth, uri("/"), current_user)
+        redirect "/users/#{current_user.username}/edit"
+      else
         session[:uid] = auth['uid']
         session[:provider] = auth['provider']
         session[:name] = auth['user_info']['name']
@@ -255,14 +240,22 @@ class Rstatus < Sinatra::Base
         session[:description] = auth['user_info']['description']
         session[:image] = auth['user_info']['image']
         session[:email] = auth['user_info']['email']
+        #let's store their oauth stuff so they don't have to re-login after
+        session[:oauth_token] = auth['credentials']['token']
+        session[:oauth_secret] = auth['credentials']['secret']
+        
+        ## We can probably get rid of this since the user confirmation will check for duplicate usernames [brimil01]
+        if User.first :username => auth['user_info']['nickname'] or auth['user_info']['nickname'] =~ /profile[.]php[?]id=/
+          #we have a username conflict!
       
-        flash[:notice] = "Sorry, someone has that name."
-        redirect '/users/new'
-        return
-      else
-        # Redirect to confirm page to verify username and provide email
-        redirect '/users/confirm'
-        return
+          flash[:notice] = "Sorry, someone has that name."
+          redirect '/users/new'
+          return
+        else
+          # Redirect to confirm page to verify username and provide email
+          redirect '/users/confirm'
+          return
+        end
       end
     end
     
@@ -274,14 +267,10 @@ class Rstatus < Sinatra::Base
       @auth.save
     end
 
-    if logged_in?
-      redirect "/users/#{current_user.username}edit"
-    else
-      session[:user_id] = @auth.user.id
+    session[:user_id] = @auth.user.id
 
-      flash[:notice] = "You're now logged in."
-      redirect '/'      
-    end
+    flash[:notice] = "You're now logged in."
+    redirect '/'      
   end
 
   get '/auth/failure' do
