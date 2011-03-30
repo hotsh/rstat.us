eb0c9a7faaf220fc5b4aac3c0fa40f300647e33c
burningTyger <m8r-0478r31@safetymail.info>
2011-03-30 22:10:28 +0200

    stay on same route after post /update

--- test/rstatus_test.rb
+++ test/rstatus_test.rb
@@ -130,6 +130,30 @@ class RstatusTest < MiniTest::Unit::TestCase
     assert_match "class='hentry mention update'", page.body    
   end
 
+  def test_user_stays_on_same_route_after_post_update
+    u = Factory(:user)
+    a = Factory(:authorization, :user => u)
+
+    log_in(u, a.uid)
+
+    visit "/updates"
+    fill_in "text", :with => "Teststring fuer die Ewigkeit ohne UTF-8 Charakter"
+    VCR.use_cassette('publish_to_hub') {click_button "Share"}
+
+    assert_match "/updates", page.current_url
+
+    visit "/replies"
+    fill_in "text", :with => "Bratwurst mit Pommes rot-weiss"
+    VCR.use_cassette('publish_to_hub') {click_button "Share"}
+
+    assert_match "/replies", page.current_url
+    
+    visit "/"
+    fill_in "text", :with => "Buy a test string. Your name in this string for only 1 Euro/character"
+    VCR.use_cassette('publish_to_hub') {click_button "Share"}
+
+    assert_match "/", page.current_url    
+  end
 
 
   def test_user_can_see_world
