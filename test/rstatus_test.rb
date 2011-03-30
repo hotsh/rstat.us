d38331c44461c342f45c5dbfbf9c47394e49535a
burningTyger <m8r-0478r31@safetymail.info>
2011-03-30 21:25:52 +0200

    highlight all mentions of username, even in body

--- test/rstatus_test.rb
+++ test/rstatus_test.rb
@@ -113,6 +113,25 @@ class RstatusTest < MiniTest::Unit::TestCase
     assert_match "@#{u.username}", page.body
   end
 
+  def test_user_can_see_replies_with_css_class_mentioned
+    u = Factory(:user)
+    a = Factory(:authorization, :user => u)
+
+    u2 = Factory(:user)
+    a2 = Factory(:authorization, :user => u2)
+    u2.feed.updates << Factory(:update, :text => "@#{u.username} Hey man.")
+    u.feed.updates << Factory(:update, :text => "some text @someone, @#{u2.username} Hey man.")
+    log_in(u, a.uid)
+    visit "/updates"
+    assert_match "class='hentry mention update'", page.body
+    
+    log_in(u2, a2.uid)
+    visit "/updates"
+    assert_match "class='hentry mention update'", page.body    
+  end
+
+
+
   def test_user_can_see_world
     u = Factory(:user)
     a = Factory(:authorization, :user => u)
