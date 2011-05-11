76097853c36119074762c4f20605c022084a2cc3
Mark Kremer <mark@without-brains.net>
2011-05-11 05:48:55 +0200

    Added Sinatra::Logging module to enable configurable logging
    Included class macro log_requests to log all requests at INFO level by
    default

--- helpers.rb
+++ helpers.rb
@@ -58,6 +58,34 @@ module Sinatra
     end
   end
 
+  module Logging
+    module Helpers
+      def logger
+        self.class.logger
+      end
+    end
+
+    @@logger = nil
+    def logger
+      ENV['LOG_SHIFT_AGE'] ||= '10'
+      unless @@logger
+        @@logger = Logger.new(File.join(File.dirname(__FILE__), "log", "#{settings.environment}.log"), ENV['LOG_SHIFT_AGE'].to_i, ENV['LOG_SHIFT_SIZE'] ? ENV['LOG_SHIFT_SIZE'].to_i : nil)
+        @@logger.level = Logger.const_get(ENV['LOG_LEVEL']) if ENV['LOG_LEVEL']
+        @@logger.formatter = proc {|severity, datetime, progname, msg| "[#{datetime}] #{severity}  #{msg}\n"}
+      end
+      @@logger
+    end
+
+    def log_requests
+      before { logger.info("#{request.request_method} #{request.route}") }
+    end
+
+    def self.registered(app)
+      app.helpers Sinatra::Logging::Helpers
+    end
+  end
+
+  register Logging
   helpers UserHelper
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,12 @@ require 'bundler'
 Bundler.setup
 Bundler.require(:default, :test)
 
+config = YAML.load_file('config/config.yml')[ENV['RACK_ENV']]
+
+config.each do |key, value|
+  ENV[key] = value
+end
+
 require_relative 'factories'
 require_relative '../rstatus'
 
