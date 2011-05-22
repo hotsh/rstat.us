128be175cfb84b7a098d1bb8c8d2530753e742ef
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-05-22 14:15:23 -0700

    Adding small ammounts of documentation, and renaming, as well as organization

--- models/author.rb
+++ models/author.rb
@@ -5,18 +5,17 @@
 class Author
   include MongoMapper::Document
 
-  DEFAULT_AVATAR = "http://rstat.us/images/avatar.png"
+  GRAVATAR               = "gravatar.com"
+  DEFAULT_AVATAR         = "http://rstat.us/images/avatar.png"
   ENCODED_DEFAULT_AVATAR = URI.encode_www_form_component(DEFAULT_AVATAR)
 
-  GRAVATAR_HOST  = "gravatar.com"
-
   # We've got a bunch of data that gets stored in Author. And basically none
-  # of it is validated right now. Fun. Then again, not all of it is neccesary.
-  key :username, String
-  key :name, String
-  key :email, String
-  key :website, String
-  key :bio, String
+  # of it is val*idated right now. Fun. Then again, not all of it is neccesary.
+  key :username,  String
+  key :name,      String
+  key :email,     String
+  key :website,   String
+  key :bio,       String
   key :image_url, String
 
   # as we said, an Author has a Feed that they're the... author of. And if
@@ -28,43 +27,73 @@ class Author
   key :remote_url, String
 
   # This takes results from an omniauth reponse and generates an author
-  def self.create_from_hash!(hsh)
+  def self.create_from_hash!(hash)
+
+    # Omniauth user information, as a hash
+    user  = hash['user_info']
+
+    # Grabs each of the important user details
+    name       = user['name']
+    username   = user['username']
+    website    = user['urls']['Website']
+    bio        = user['description']
+    image      = user['image']
+    remote     = user['url']
+
+    # Creates an Author object with the details
     create!(
-      :name => hsh['user_info']['name'],
-      :username => hsh['user_info']['nickname'],
-      :website => hsh['user_info']['urls']['Website'],
-      :bio => hsh['user_info']['description'],
-      :image_url => hsh['user_info']['image'],
-      :remote_url => hsh['user_info']['url']
+      name:       name,
+      username:   username,
+      website:    website,
+      bio:        bio,
+      image_url:  image,
+      remote_url: remote
     )
   end
 
+  # Returns a remote url, or the regular user url
   def url
-    return remote_url if remote_url
-    "/users/#{username}"
+    if remote_url.present?
+      remote_url
+    else
+      "/users/#{username}"
+    end
   end
 
-  # We've got a couple of options here. If they have some sort of image from
-  # Facebook or Twitter, we use that, and if they don't, we go with Gravatar.
-  # If none of that is around, then we show the DEFAULT_AVATAR
+  # Determine the avatar url and return it
   def avatar_url
-    return image_url      if image_url
-    return DEFAULT_AVATAR if email.nil?
 
-    gravatar_url
+    # If the user has a facebook or twitter image, return it
+    if image_url.present?
+      image_url
+
+    # If the user has an email (Don't they have to?), look for a gravatar url.
+    elsif email.present?
+      gravatar_url
+
+    # Otherwise return the default avatar
+    else
+      DEFAULT_AVATAR
+    end
   end
 
+  # Determine the display name from the username or name
   def display_name
-    return username if name.nil? || name.empty?
-    name
+
+    # If the user has a name, return it
+    if name.present?
+      name
+
+    # Otherwise return the username
+    else
+      username
+    end
   end
 
+  # Return the gravatar url
+  # Query described [here](http://en.gravatar.com/site/implement/images/#default-image).
   def gravatar_url
-    "http://#{GRAVATAR_HOST}#{gravatar_path}"
-  end
-
-  # these query parameters are described [here](http://en.gravatar.com/site/implement/images/#default-image).
-  def gravatar_path
-    "/avatar/#{Digest::MD5.hexdigest(email)}?s=48&r=r&d=#{ENCODED_DEFAULT_AVATAR}"
+    email_digest = Digest::MD5.hexdigest email
+    "http://#{GRAVATAR}/avatar/#{email_digest}?s=48&r=r&d=#{ENCODED_DEFAULT_AVATAR}"
   end
 end
