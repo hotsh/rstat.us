28a369d0eba3cae1a75675041ce1e26d37f535b3
Steve Klabnik <steve@steveklabnik.com>
2011-07-17 22:20:08 -0400

    Signup url

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    u = User.create(:email => params[:email],
+                    :status => "unconfirmed")
+    u.set_perishable_token
+
+    if development?
+      puts uri("/") + "confirm/#{u.perishable_token}"
+    else
+      Notifier.send_signup_notification(params[:email], u.perishable_token)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,23 @@ RstatUs::Application.routes.draw do
   root :to => "static#homepage", :constraints => lambda {|x| x.session[:user_id] == nil}
   root :to => "dashboard#index", :constraints => lambda {|x| x.session[:user_id] != nil}
 
+  # Sessions
   resources :sessions, :only => [:new, :create, :destroy]
-
   match "/login", :to => "sessions#new"
   match "/logout", :to => "sessions#destroy"
 
   get "static/follow"
 
+  # Static
   match "contact" => "static#contact"
   match "open_source" => "static#open_source"
   match "help" => "static#help"
 
-  # AUTH
+  # External Auth
   match '/auth/:provider/callback', :to => 'auth#auth'
   match '/auth/failure', :to => 'auth#failure'
   match '/users/:username/auth/:provider', :via => :delete, :to => "auth#destroy"
+
+  # Users
+  resources :users, :only => [:create]
 end
