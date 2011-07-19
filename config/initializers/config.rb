ecb8ef17fc73fe60c47cca3cd62e1cf30e1e5caa
Steve Klabnik <steve@steveklabnik.com>
2011-07-18 20:13:12 -0400

    Config derp.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,5 +1,5 @@
 unless Rails.env.production?
-  config = YAML.load_file(File.join(Rails.root + '/config/config.yml'))[ENV['RAILS_ENV']]
+  config = YAML.load_file(File.expand_path(File.join(Rails.root, '/config/config.yml')))[Rails.env]
 
   config.each do |key, value|
     ENV[key] = value
