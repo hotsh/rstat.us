c8752833033170e6016aa34cbe28080809f57794
Steve Klabnik <steve@steveklabnik.com>
2011-08-30 20:42:50 -0400

    Copy sample config if it doesn't exist

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,5 +1,12 @@
+require 'fileutils'
+
 unless Rails.env.production?
-  config = YAML.load_file(File.expand_path(File.join(Rails.root, '/config/config.yml')))[Rails.env]
+  config_file = File.expand_path(File.join(Rails.root, '/config/config.yml'))
+  config_file_sample = File.expand_path(File.join(Rails.root, '/config/config.yml.sample'))
+
+  FileUtils.cp(config_file_sample, config_file) unless File.exists?(config_file)
+
+  config = YAML.load_file(config_file)[Rails.env]
 
   config.each do |key, value|
     ENV[key] = value
