d02afa97d0f8707ece276777966ce5c7bb5d8459
Steve Klabnik <steve@steveklabnik.com>
2011-09-11 23:28:18 -0400

    we don't need all

--- app/models/all.rb
+++
@@ -1,8 +0,0 @@
-# This file is a simple helper file that lets you require every model at once.
-
-require_relative 'author'
-require_relative 'update'
-require_relative 'authorization'
-require_relative 'user'
-require_relative 'notifier'
-require_relative 'feed'
