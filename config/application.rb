0970835af066c8e6438bb5d3a686ad113b7b404b
Jamie Gaskins <jgaskins@gmail.com>
2011-09-14 07:11:09 +0800

    Require sprockets in application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+require "sprockets/railtie"
 
 # We don't want test_unit or tasks like db:test:prepare, so we commented out
 # require "rails/test_unit/railtie"
