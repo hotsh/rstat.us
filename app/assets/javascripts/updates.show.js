e31c2d459887e8bce0be0a2294424aa956eda2c2
Mark Zalar <mark.zalar@gmail.com>
2012-01-15 13:33:24 -0500

    removed duplicate confirmation dialog js code - the functionality in the removed updates.show.js is still in main.js.coffee

--- app/assets/javascripts/updates.show.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  $(document).ready(function() {
-    return $(".remove-update").click(function() {
-      return confirm("Are you sure you want to delete this update?");
-    });
-  });
-}).call(this);
