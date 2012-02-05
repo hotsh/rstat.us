848421b6e3ef30a54c03aec931b6d2924d29df97
Chris Geihsler <chris@geihsler.net>
2012-02-05 16:02:35 -0500

    refactored finger test

--- test/lib/finger_test.rb
+++ test/lib/finger_test.rb
@@ -29,29 +29,24 @@ describe "when querying web finger" do
     url_link = Link.new('http://schemas.google.com/g/2010#updates-from', 'http://feed.url')
     public_key_link = Link.new('magic-public-key', 'ignored,key')
     salmon_link = Link.new('salmon', 'http://salmon.url')
-    links = [ url_link, public_key_link, salmon_link ]
-    xrd = Xrd.new(links)
+    xrd = Xrd.new([ url_link, public_key_link, salmon_link ])
 
     @email = "someone@somewhere.com"
 
     Redfinger.expects(:finger).with(@email).returns(xrd)
+
+    @finger_data = QueriesWebFinger.query(@email)
   end
 
   it "should get the remote url" do
-    finger_data = QueriesWebFinger.query(@email)
-
-    assert_equal "http://feed.url", finger_data.url
+    assert_equal "http://feed.url", @finger_data.url
   end
   
   it "should get public key " do
-    finger_data = QueriesWebFinger.query(@email)
-
-    assert_equal "key", finger_data.public_key
+    assert_equal "key", @finger_data.public_key
   end
 
   it "should get salmon url" do
-    finger_data = QueriesWebFinger.query(@email)
-
-    assert_equal "http://salmon.url", finger_data.salmon_url
+    assert_equal "http://salmon.url", @finger_data.salmon_url
   end
 end
