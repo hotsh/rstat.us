cf42e5956e74f2cfa6746cee8657704c31ac41f3
Carol Nichols <carol.nichols@gmail.com>
2012-03-31 22:28:38 -0400

    Changing the one method in a helper to be a decorator method

--- app/decorators/author_decorator.rb
+++ app/decorators/author_decorator.rb
@@ -1,6 +1,15 @@
 class AuthorDecorator < ApplicationDecorator
   decorates :author
 
+  def avatar
+    h.content_tag "div", :class => "avatar" do
+      h.link_to(
+        h.image_tag(avatar_src, :class => "photo", :alt => "avatar"),
+        author.url
+      )
+    end
+  end
+
   def website_link
     url = if model.website[0,7] == "http://" or model.website[0,8] == "https://"
             model.website
@@ -9,5 +18,15 @@ class AuthorDecorator < ApplicationDecorator
           end
 
     h.link_to(url, url, :rel => 'me', :class => 'url')
+  end
+
+  private
+
+  def avatar_src
+    if author.avatar_url.eql? Author::DEFAULT_AVATAR
+      h.asset_path(author.avatar_url)
+    else
+      author.avatar_url
+    end
   end
 end
--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def avatar_for(author)
-    if author.avatar_url.eql? Author::DEFAULT_AVATAR
-      asset_path(author.avatar_url)
-    else
-      author.avatar_url
-    end
-  end
-end
