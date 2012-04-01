484382d88a4040964e2f60432a8bdd379bd7178f
buddhamagnet <buddhamagnet@gmail.com>
2012-04-01 11:37:22 +0100

    added tests for user search regex processing

--- test/acceptance/user_search_test.rb
+++ test/acceptance/user_search_test.rb
@@ -26,6 +26,20 @@ describe "user search" do
     visit "/users?search=ebr"
     assert has_content?("zebra")
   end
+  
+  it "copes an with asterisk in the search string" do
+    visit "/users?search=*"
+    assert_equal 200, page.status_code, "search input (*) caused a #{page.status_code} response"
+    assert has_content?("Please enter a valid search term"), "search input (*) did not cause a flash message"
+  end
+
+  it "copes with URL-escaped input in the search string" do
+    %w[ ? + {1} {1,} {1,2}].each do |n|
+      visit "/users?search=#{CGI.escape(n)}"
+      assert_equal 200, page.status_code, "search input #{n} caused a #{page.status_code} response"
+      assert has_content?("Please enter a valid search term"), "search input #{n} did not cause a flash message"
+    end
+  end
 
   it "finds users case-insensitively" do
     visit "/users?search=ZEBR"
