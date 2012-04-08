2bf4139cd518a4d0a95900682cc02834f9ab3c28
Carol Nichols <carol.nichols@gmail.com>
2012-04-08 18:57:45 -0400

    Refactoring so that instantiation of a SalmonInterpreter validates the data

--- app/models/salmon_interpreter.rb
+++ app/models/salmon_interpreter.rb
@@ -1,11 +1,16 @@
 class SalmonInterpreter
-  def self.interpret_entry(body, params = {})
-    raise(ArgumentError, "request body can't be empty") if body.empty?
+  def initialize(body, params = {})
+    raise(ArgumentError, "request body can't be empty") if !body || body.empty?
 
-    feed       = SalmonInterpreter.find_feed(params[:feed_id])
-    salmon     = SalmonInterpreter.parse(body)
+    @feed = SalmonInterpreter.find_feed(params[:feed_id])
 
-    raise(ArgumentError, "can't parse salmon envelope") if salmon.nil?
+    @salmon = SalmonInterpreter.parse(body)
+    raise(ArgumentError, "can't parse salmon envelope") if @salmon.nil?
+
+    @root_url = params[:root_url]
+  end
+
+  def interpret
   end
 
   # Isolating calls to external classes so we can stub these methods in test
--- test/models/salmon_interpreter_test.rb
+++ test/models/salmon_interpreter_test.rb
@@ -3,44 +3,44 @@ require 'mocha'
 
 require_relative '../../app/models/salmon_interpreter'
 
-# Stubbing methods for when they're not interesting to the current test
-class SalmonInterpreter
-  def self.find_feed(id)
-    true
-  end
-
-  def self.parse(body)
-    true
-  end
-end
-
 module MongoMapper
   class Error < StandardError; end
   class DocumentNotFound < Error; end
 end
 
 describe "SalmonInterpreter" do
-  it "raises MongoMapper::DocumentNotFound if the feed doesnt exist" do
-    id = 99999
-    SalmonInterpreter.expects(:find_feed).with(id).raises(MongoMapper::DocumentNotFound)
-    lambda {
-      SalmonInterpreter.interpret_entry("request body text", :feed_id => id)
-    }.must_raise MongoMapper::DocumentNotFound
+  describe "#new" do
+    it "raises MongoMapper::DocumentNotFound if the feed doesnt exist" do
+      id = 99999
+      SalmonInterpreter.stubs(:parse)
+      SalmonInterpreter.expects(:find_feed)
+                       .with(id)
+                       .raises(MongoMapper::DocumentNotFound)
+      lambda {
+        SalmonInterpreter.new("something", :feed_id => id)
+      }.must_raise MongoMapper::DocumentNotFound
+    end
+
+    it "raises an ArgumentError if the body is empty string" do
+      lambda {
+        SalmonInterpreter.new("")
+      }.must_raise ArgumentError
+    end
+
+    it "raises an ArgumentError if the body is nil" do
+      lambda {
+        SalmonInterpreter.new(nil)
+      }.must_raise ArgumentError
+    end
+
+    it "raises an ArgumentError if we can't parse the body" do
+      body = "<?xml version='1.0' encoding='UTF-8'?><not-salmon-xml />"
+      SalmonInterpreter.expects(:parse).with(body).returns(nil)
+      SalmonInterpreter.stubs(:find_feed)
+
+      lambda {
+        SalmonInterpreter.new(body)
+      }.must_raise ArgumentError
+    end
   end
-
-  it "raises an ArgumentError if the body is empty" do
-    lambda {
-      SalmonInterpreter.interpret_entry("")
-    }.must_raise ArgumentError
-  end
-
-  it "raises an ArgumentError if we can't parse the body" do
-    body = "<?xml version='1.0' encoding='UTF-8'?><not-salmon-xml />"
-    SalmonInterpreter.expects(:parse).with(body).returns(nil)
-
-    lambda {
-      SalmonInterpreter.interpret_entry(body)
-    }.must_raise ArgumentError
-  end
-
 end
