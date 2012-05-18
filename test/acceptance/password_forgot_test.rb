8d25070ce2aaa22c937e46acd5827b195e784a44
Carol Nichols <carol.nichols@gmail.com>
2012-05-18 16:36:09 -0400

    Adding some tests for #539, checking for something like an email before trying to send a password reset email

--- test/acceptance/password_forgot_test.rb
+++ test/acceptance/password_forgot_test.rb
@@ -30,5 +30,23 @@ describe "forgotten password" do
       assert has_content? "A link to reset your password has been sent to someone@somewhere.com."
     end
   end
+
+  it "says try again if you don't enter anything in the email field" do
+    visit "/forgot_password"
+    click_button "Send"
+
+    within flash do
+      assert has_content? "You didn't enter a correct email address. Please check your email and try again."
+    end
+  end
+
+  it "says try again if you enter something that isn't an email address" do
+    visit "/forgot_password"
+    fill_in "email", :with => "i like to fill in forms"
+    click_button "Send"
+
+    within flash do
+      assert has_content? "You didn't enter a correct email address. Please check your email and try again."
+    end
   end
 end
