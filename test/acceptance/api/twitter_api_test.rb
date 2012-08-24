ebca9fc90306285a985cb0ae623491b9e1465207
AriT93 <arit93@gmail.com>
2012-08-23 21:24:50 -0500

    Twitter-api    Tests for api methods  #603
    - acceptance testing for the api methods that have already been
      implemented.
    - cleaned up require_relative backport to 1.8 line

--- test/acceptance/api/twitter_api_test.rb
+++ test/acceptance/api/twitter_api_test.rb
@@ -1,4 +1,3 @@
-require 'require_relative' if RUBY_VERSION[0,3] == '1.8'
 require_relative '../acceptance_helper'
 
 describe "twitter-api" do
@@ -6,19 +5,64 @@ describe "twitter-api" do
 
   describe "user_timeline" do
     it "returns valid json" do
+
       log_in_as_some_user
+
       u = Fabricate(:user)
-      5.times do
+
+      5.times do |index|
         update = Fabricate(:update,
-                         :author => u.author
+                           :text   => "Update test is #{index}",
+                           :twitter => true,
+                           :author => u.author
                          )
-        u.feed.updates << update
+       u.feed.updates << update
       end
 
       visit "/api/statuses/user_timeline.json?screen_name=#{u.username}"
 
       parsed_json = JSON.parse(source)
       parsed_json.length.must_equal 5
+      parsed_json[0]["text"].must_equal("Update test is 4")
+
     end
   end
+
+  describe "home_timeline" do
+    it "it returns the home timeline for the user" do
+      u = Fabricate(:user)
+      log_in_username u
+
+      update = Fabricate(:update,
+                         :text => "Hello World I'm on RStatus",
+                         :author => u.author)
+      u.feed.updates << update
+
+      visit "/api/statuses/home_timeline.json"
+
+      parsed_json = JSON.parse(source)
+      parsed_json[0]["text"].must_equal(update.text)
+
+    end
+  end
+
+  describe "mentions" do
+    it "gives mentions" do
+      skip "unimplemented"
+
+      u = Fabricate(:user)
+      log_in_username u
+
+      update = Fabricate(:update,
+                         :text => "@#{u.username} How about them Bears",
+                         :author => u.author)
+
+      visit "/api/statuses/mention.json"
+
+      parsed_json = JSON.parse(source)
+      parsed_json[0]["text"].must_equal(update.text)
+
+    end
+  end
+
 end
