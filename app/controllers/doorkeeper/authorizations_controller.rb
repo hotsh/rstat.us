d3bd8cffd4f231a8a0b1be7174af1c8ed053a69f
Carol Nichols <carol.nichols@gmail.com>
2012-10-19 08:38:43 -0400

    Override doorkeeper controllers and views
    So that we can use rstatus' layout and have API applications owned by a user and only viewable/editable by the owner.

---
+++ app/controllers/doorkeeper/authorizations_controller.rb
@@ -0,0 +1,48 @@
+class Doorkeeper::AuthorizationsController < ::ApplicationController
+  include Doorkeeper::Helpers::Controller
+  before_filter :authenticate_resource_owner!
+
+  def new
+    if authorization.valid?
+      if authorization.access_token_exists?
+        authorization.authorize
+        redirect_to authorization.success_redirect_uri
+      end
+    elsif authorization.redirect_on_error?
+      redirect_to authorization.invalid_redirect_uri
+    else
+      @error = authorization.error_response
+      render :error
+    end
+  end
+
+  def create
+    if authorization.authorize
+      redirect_to authorization.success_redirect_uri
+    elsif authorization.redirect_on_error?
+      redirect_to authorization.invalid_redirect_uri
+    else
+      @error = authorization.error_response
+      render :error
+    end
+  end
+
+  def destroy
+    authorization.deny
+    redirect_to authorization.invalid_redirect_uri
+  end
+
+  private
+
+  def authorization_params
+    params.has_key?(:authorization) ? params[:authorization] : params
+  end
+
+  def client
+    @client ||= Doorkeeper::OAuth::Client.find(authorization_params[:client_id])
+  end
+
+  def authorization
+    @authorization ||= Doorkeeper::OAuth::AuthorizationRequest.new(client, current_resource_owner, authorization_params)
+  end
+end
