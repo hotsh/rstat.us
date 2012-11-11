683070b8a6caec3d0d7d396f81ced4c69dee7ee1
Gavin Laking <gavinlaking@gmail.com>
2012-11-11 15:57:40 -0500

    Alphabetize static actions and routes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,15 +1,6 @@
 class StaticController < ApplicationController
   before_filter :require_user, :only => :follow
 
-  def homepage
-    @list_class = ""
-    render :layout => false
-  end
-
-  def open_source
-    @title = "open source"
-  end
-
   def about
     @title = "about us"
   end
@@ -24,4 +15,14 @@ class StaticController < ApplicationController
 
   def help
   end
+
+  def homepage
+    @list_class = ""
+    render :layout => false
+  end
+
+  def open_source
+    @title = "open source"
+  end
+
 end
